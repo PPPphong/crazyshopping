48da2e89cfb60f6c68c81c26e9df3a8e3daf95d2
phong <2358935481@qq.com>
2023-11-09 17:04:48 +0800

    升级A

---
+++ src/components/GoodsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <goods></goods>
+  <goods></goods>
+  <goods></goods>
+</template>
+
+<script>
+import Goods from "@/components/Goods.vue";
+export default {
+  name: "GoodsList",
+  components: {Goods}
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import UploadGoods from '@/components/UploadGoods.vue'
-import Goods from '@/components/Goods.vue'
 import ChangePwd from '@/components/ChangePwd.vue'
 import OrdersManager from '@/components/OrdersManager.vue'
 import Login from "@/components/login.vue";
@@ -13,13 +12,14 @@ import register from '@/components/register.vue'
 import UserInfo from '@/components/UserInfo.vue'
 import Orders from '@/components/Orders.vue'
 import EditUserInfo from '@/components/EditUserInfo.vue'
+import GoodsList from '@/components/GoodsList.vue'
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
-        { path : '/',redirect: 'goods'},
+        { path : '/',redirect: 'GoodsList'},
         { path : '/login',component: Login},
-        { path: '/goods', component: Goods },
+        { path: '/GoodsList', component: GoodsList },
         { path: '/EditGoods',component:EditGoods},
         { path: '/uploadgoods', component: UploadGoods },
         { path: '/changepwd', component: ChangePwd },
